63fcb9efddcb68a85d1c8eb64890333abcfc65cb
Ernest Stratoberdha <stratoberdha@users.noreply.github.com>
2015-07-28 15:27:22 -0400

    Script to gather system usage (CPU+Memory)

---
+++ psutils.py
@@ -0,0 +1,48 @@
+# demonstrate psutil for memory and cpu usages
+import sys
+import subprocess
+import psutil as ps
+
+for x in range(0, 10):
+    cpu = ps.cpu_percent(interval=2)
+    mem = ps.virtual_memory()
+    m = mem.used >> 20
+     
+    mfile = "/home/nestig/monpi/logs/memory.txt"
+    cfile = "/home/nestig/monpi/logs/cpu.txt"
+    space = " "
+    newline = '\n'
+    y  = x * 5 # arbitrarily set loop variable to get samples
+    
+    # open memory log file
+    openfile = open(mfile, 'a')      
+    openfile.write(str(y))
+    openfile.write(space)
+    openfile.write(str(m))
+    openfile.write('\n')
+    openfile.close()
+    
+    # open cpu log file
+    openfile = open(cfile, 'a')
+    openfile.write(str(y))
+    openfile.write(space)
+    openfile.write(str(cpu))
+    openfile.write('\n')
+    openfile.close()
+    
+    print "Try: ",  x
+     
+
+# call filedata_graph.py to plot
+subprocess.call(['python /home/nestig/monpi/filedata_graph.py'], shell=True)
+
+#subprocess.call(['cat /dev/null > /home/nestig/monpi/logs/network.txt'], shell=True)
+#subprocess.call(['cat /dev/null > /home/nestig/monpi/logs/memory.txt'], shell=True)
+#subprocess.call(['cat /dev/null > /home/nestig/monpi/logs/cpu.txt'], shell=True)
+
+    #print m
+    # print the data to the screen
+    #print "CPU usage: ", cpu
+    #print "Memory usage: ", mem.used >> 20
+
+
