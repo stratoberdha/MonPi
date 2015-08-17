0067fb3aa762867503e5d58b9fd84f4018866895
Ernest Stratoberdha <stratoberdha@users.noreply.github.com>
2015-08-16 22:55:06 -0400

    The updated version of harvest.py

--- netcap.py
+++ netcap.py
@@ -1,34 +1,49 @@
+"""netcap.py: Captures packets from the network traffic on eth0 interface."""
+
+# Import os.path, subprocess and sys libraries to call other scripts and use system calls.
 import subprocess
 import sys
 import os.path
-import psutil
 
+# Useful message to display when the script is running (for debugging purposes).
 print "Monitoring broadcast packets on the network..."
 
-# loop range set arbitrarily for testing
+# Start collecting network usage statistics using an arbitrarily set range loop:
 for x in range(0, 10): 
-
+    
+    # Start capturing with tshark on eth0 interface and write the raw data output to capfile under root. 
     subprocess.call(['sudo tshark -i eth0 -a duration:2 -w /root/capfile'], shell=True)
-    # moving file to different location to avoid root permission problem
-    subprocess.call(['sudo mv /root/capfile /home/nestig/monpi/capfile'], shell=True)
-    # next 3 lines are calls to process output formatting of packets captured
+    
+    # Neccessary to move file to different location to avoid root permission problem
+    subprocess.call(['sudo mv /root/capfile ./capfile'], shell=True)
+    
+    # Next 3 lines are calls to process output formatting of network packets captured
     pcap = subprocess.Popen(["sudo", "capinfos", "-yu", "/home/nestig/monpi/capfile"], stdout=subprocess.PIPE)
     lrow = subprocess.Popen(["awk",  "NR==3"], stdin=pcap.stdout, stdout=subprocess.PIPE)
     dbrate = subprocess.Popen (["cut",  "-c22-25"], stdin=lrow.stdout, stdout=subprocess.PIPE)
-    pcap.stdout.close() # close output stream
-    lrow.stdout.close()  # close output stream
-    stdout, stderr = dbrate.communicate() # connects PIPE
-    sys.stdout.write(stdout) # no newline bullshit
-    infile = "/home/nestig/monpi/logs/network.txt" # path to the input file to be modified
-    openfile = open(infile, 'a')
-    space = " " 
-    y = x * 5	# holds approximate intervals
     
-    # write the formatted data to file
+    # Close output stream
+    pcap.stdout.close() 
+    lrow.stdout.close()
+
+    # Connect PIPE  
+    stdout, stderr = dbrate.communicate()
+
+    # To avoid newline
+    sys.stdout.write(stdout)
+
+    # The infile will hold the network usage data to server as input file.
+    infile = "./logs/network.txt"
+    openfile = open(infile, 'a')
+    space = " "
+    # Collecting data every 5 seconds. 
+    y = x * 5
+    
+    # Write the formatted data to file
     openfile.write(str(y))
     openfile.write(space)    
     openfile.write(stdout)
     openfile.close()	
 
-# call psutils.py to get memory and cpu stats
+# Call psutils.py script to collect system usage statistics.
 subprocess.call(['python /home/nestig/monpi/psutils.py'], shell=True)
